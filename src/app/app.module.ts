e47937d31b2cfcbe1ab7dfc648206aca79225773
fluffycheeseball <billvolestate@gmail.com>
2017-06-25 13:52:12 +0100

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import {CanvasComponent} from './canvas/component/canvas.component';
 
 @NgModule({
--- src/app/canvas/models/canvas.ts
+++ src/app/canvas/models/canvas.ts
@@ -2,20 +2,36 @@ export class Canvas {
     public Rows : number[] = [1,2, 3,4,5,6,7,8]
     public Columns : number[] = [1,2, 3,4,5,6,7,8]
 
-    Locations: string[][] = CanvasHelper.InitCanvas(this.Rows, this.Columns);
+    Locations: Location[][] = CanvasHelper.InitCanvas(this.Rows, this.Columns);
 }
 
 export class CanvasHelper {
     static InitCanvas(rows : number[], columns: number[]) {
-        var locations : string[][];
-        locations = new Array<string[]>(rows.length);
+        var locations : Location[][];
+        var isWhite: boolean = false;
+
+        locations = new Array<Location[]>(rows.length);
        
           for(var row = 0;row < rows.length; row++) {
-              locations[row] = new Array<string>(columns.length)
+              locations[row] = new Array<Location>(columns.length)
               for(var column= 0;column<columns.length;column++) {
-                  locations[row][column] = "r" + rows[row].toString() + "-c" + columns[column].toString()
-                 }
+                         locations[row][column] = new Location(row, column);
+                  }
               }
-        return locations;
+     return locations;
     }
 }
+
+export class Location {
+    public IsWhite:boolean;
+
+    constructor(public Row: number, public Column: number) {
+        this.IsWhite = (Row %2 == 0 && Column %2 !=0 ) || (Row %2 != 0 && Column %2 ==0 );
+    }
+
+
+    GetText(): string {
+        return "r" + this.Row + "c" + this.Column;
+    }
+}
+
